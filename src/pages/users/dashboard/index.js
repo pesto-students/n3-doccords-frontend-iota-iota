8e10507175966f5aa9e32d4b40a8c8f7ed2825a1
Navtej01 <iosdevnavtej@gmail.com>
2021-07-04 13:49:50 +0530

    UI of Admin dashboard is done

--- src/pages/users/dashboard/index.js
+++ src/pages/users/dashboard/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import StackedBar from "components/UserCharts/StackedBar";
 import Polar from "components/UserCharts/Polar";
 import PieChart from "components/UserCharts/Pie";
-import LineChart from "components/UserCharts/Line";
+// import LineChart from "components/UserCharts/Line";
 // import DoughnutChart from "components/UserCharts/Doughnut";
 import { Grid, Container, Paper, Box } from "@material-ui/core";
 
@@ -10,20 +10,13 @@ const UserDashboard = () => {
   return (
     <Container>
       <Grid container spacing={2} justify="spaceAround">
-        <Grid item xs={12} md={6}>
-          <Paper>
-            <Box>
-              <StackedBar />
-            </Box>
-          </Paper>
-        </Grid>
-        <Grid item xs={12} md={6}>
+        {/* <Grid item xs={12} md={6}>
           <Paper>
             <Box>
               <LineChart />
             </Box>
           </Paper>
-        </Grid>
+        </Grid> */}
         <Grid item xs={12} md={6}>
           <Paper>
             <Box>
@@ -35,6 +28,13 @@ const UserDashboard = () => {
           <Paper>
             <Box>
               <PieChart />
+            </Box>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} md={12}>
+          <Paper>
+            <Box>
+              <StackedBar />
             </Box>
           </Paper>
         </Grid>
